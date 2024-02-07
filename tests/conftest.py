03a7b4483776b2ff11cc643762d573a04277fea5
Nick Hawkins <nickhawkins@Nicks-MacBook-Pro.local>
2024-02-06 21:37:33 -0500

    Add conftest

---
+++ tests/conftest.py
@@ -0,0 +1,10 @@
+"""Test fixtures to be used for all PyTest cases."""
+import pytest
+
+
+@pytest.fixture()
+def test_data():
+    """Define a list of test data for unit tests."""
+    data = []
+
+    return data
